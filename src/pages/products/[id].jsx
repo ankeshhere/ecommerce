1da59bf80007654c8fa4ff09381ce3c60ec37e4e
hd-official-github <official.himanshudubey@gmail.com>
2023-03-13 00:22:16 +0530

    fg

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -5,19 +5,19 @@ import { createClient } from 'contentful'
 //     fallback: 'blocking' 
 //   }
 // }
-export async function getServerSideProps (context) {
+export async function getServerSideProps(context) {
   console.log('ID ', context.params.id)
-    const client = createClient({
-      space: process.env.SPACE_ID || '',
-      accessToken: process.env.TOKEN_ID || ''
-    })
+  const client = createClient({
+    space: process.env.SPACE_ID || '',
+    accessToken: process.env.TOKEN_ID || ''
+  })
   //   let vid = capitalizeFirstLetter(context.params.id).replaceAll('-', ' ')
-    const v = await client.getEntries({
-      content_type: 'product',
-      'fields.slug': context.params.id
-    })
+  const v = await client.getEntries({
+    content_type: 'product',
+    'fields.slug': context.params.id
+  })
   return {
-    props: { proddata: v.items  }
+    props: { proddata: v.items[0].fields }
   }
 }
 import { StarFilled, WhatsAppOutlined } from '@ant-design/icons'
@@ -27,7 +27,7 @@ import Product from 'components/Product'
 import SectionHeading from 'components/SectionHeading'
 import React, { useEffect, useState } from 'react'
 
-export default function Collection ({ proddata }) {
+export default function Collection({ proddata }) {
   const [windowsize, setwindowsize] = useState(0)
   useEffect(() => {
     setwindowsize(window.innerWidth)
@@ -35,14 +35,14 @@ export default function Collection ({ proddata }) {
   const handleChange = value => {
     console.log(`selected ${value}`)
   }
-  console.log("PD",proddata);
+  console.log("PD", proddata);
   return (
     <>
       <div style={{ marginTop: 30 }}>
         <div className='container' style={{ maxWidth: 1000 }}>
           <div className='row'>
             <div className='col-md-6'>
-              <ImageCarousel />
+              <ImageCarousel images={proddata.images} />
               {/* <img
                 src='https://cdn.shopify.com/s/files/1/0581/9383/8255/products/CHI1372_720x.jpg?v=1625574126'
                 alt=''
@@ -50,8 +50,8 @@ export default function Collection ({ proddata }) {
               /> */}
             </div>
             <div className='col-md-6 py-2 pcont'>
-              <div className='bcrum'>Home / Fearless & Bold Handbags</div>
-              <h4>BURNT ORANGE CHECKS HANDBAG</h4>
+              <div className='bcrum'>Home / {proddata.collection[0].fields.collectionName} / {proddata.name}</div>
+              <h4>{proddata.name}</h4>
               <div className='star'>
                 <Rate disabled defaultValue={5} />
                 {/* <StarFilled style={{color:"#7c2325",paddingRight:4}}/>
@@ -60,7 +60,7 @@ export default function Collection ({ proddata }) {
                 <StarFilled style={{color:"#7c2325",paddingRight:4}}/>
                 <StarFilled style={{color:"#7c2325",paddingRight:4}}/> */}
               </div>
-              <p>Rs. 2,399.00</p>
+              <p>Rs. {proddata.price}</p>
               <span className='spa'>Inclusive of all taxes.</span>
               <div className='divider cox'></div>
 
@@ -68,28 +68,26 @@ export default function Collection ({ proddata }) {
                 <WhatsAppOutlined style={{ marginRight: 10 }} /> Enquire on
                 Whatsapp
               </a>
-              <div className='coup'>
-                <span>Flat 10% off</span>
-                <span>
-                  Use Coupon: <strong>SHUBV200</strong>{' '}
-                </span>
-              </div>
+              {
+                proddata.discount_prc && Object.keys(proddata.discount_prc).length > 0 && <div className='coup'>
+                  <span>Flat {proddata.discount_prc.perc} off</span>
+                  <span>
+                    Use Coupon: <strong>{proddata.discount_prc.code}</strong>{' '}
+                  </span>
+                </div>
+
+              }
+
               <p className='desc'>
-                To all you women out there, be bold & be fearless. And while you
-                do this bag is the perfect bag that you’ll be wanting to keep
-                besides you to remind you of that every single day.<br></br>
-                <br></br> Made from the finest weaves of cotton, this handbag is
-                a perfect combination of a classic & a stylish bag. Comes with
-                adjustable belts, two partitions inside the bag and a back
-                pocket.
+                {proddata.description}
               </p>
 
               <div className='row d-flex justify-content-center align-items-center cc my-4'>
                 <div className='col-6 sz'>Size</div>
-                <div className='col-6 sz'>218”(w)x14”(h)x5”(b)22</div>
+                <div className='col-6 sz'>{proddata.size}</div>
                 <div className='divider'></div>
                 <div className='col-6 sz'>Colour</div>
-                <div className='col-6 sz'>Burnt orange</div>
+                <div className='col-6 sz'>{proddata.color}</div>
               </div>
             </div>
           </div>
