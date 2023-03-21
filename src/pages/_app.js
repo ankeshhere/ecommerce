a87ddbaa831f6baf4e129e51e78d0f0e45bf6836
hd-official-github <official.himanshudubey@gmail.com>
2023-03-22 00:28:53 +0530

    ffg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import Layout from 'components/layout'
 import { Montserrat } from '@next/font/google'
 import { ConfigProvider } from 'antd'
+import Script from 'next/script'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -16,6 +17,18 @@ export default function App({ Component, pageProps }) {
         },
       }}
     >
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LL3D33GT8C"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments)}
+         gtag('js', new Date());
+         gtag('config', 'G-LL3D33GT8C');
+        `}
+      </Script>
       <Layout>
         <div className={montserrat.className}>
           <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,13 +6,13 @@ export default function Document() {
       {/* <Head /> */}
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LL3D33GT8C"></script>
-        <script>
+        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-LL3D33GT8C"></script> */}
+        {/* <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
           gtag('config', 'G-LL3D33GT8C');
-        </script>
+        </script> */}
       </Head>
       <body>
         <Main />
