ecc780fbd98bb1002a7e4a334f0c3328825a2c50
hd-official-github <official.himanshudubey@gmail.com>
2025-02-18 00:20:34 +0530

    category and products

--- src/app/[category]/page.js
+++ src/app/[category]/page.js
@@ -1,5 +1,55 @@
+import Product from "@/components/product";
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Category({ params }) {
     const { category } = await params;
 
-    return <div>Post: {category}</div>;
+    return (
+        <div>
+            <section className="relative h-[400px] max-w-[1300px] mx-auto">
+                <Image src={'/banner2.jpg'} fill className="object-cover" alt="" />
+            </section>
+
+            <section className="max-w-[1300px] mx-auto">
+                <h2 className="text-4xl font-heading text-primary font-bold text-center my-6">{category.split('-')[0].charAt(0).toUpperCase() + category.split('-')[0].slice(1)} Wear</h2>
+
+                <p className="text-sm text-center text-gray-800 max-w-[700px] p-4 mx-auto">Welcome to the charming world, where little ones step into the realm of style and tradition. Manyavar, a brand celebrated for its exquisite ethnic wear, now brings an array of Indo-western, kurta jacket sets, and kurta pajamas designed exclusively for boys aged 2-14 years. This collection is a colourful playground of fashion, blending traditional elegance with playful trends, perfect for the young fashion enthusiasts.
+                </p>
+                <div className="flex justify-center my-12 flex-row max-w-[1300px] mx-auto gap-4 flex-wrap">
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                    <Link href={''} className="border border-primary px-8 font-bold font-heading py-2 rounded-md hover:bg-primary hover:text-white   duration-150 transition-all">Collection 1</Link>
+                </div>
+            </section>
+            <div className="h-[1px] bg-gray-800 max-w-[700px] mx-auto"></div>
+
+
+
+            <section className="max-w-[1300px] mx-auto my-12">
+                <h3 className="text-primary font-heading font-bold text-center text-2xl  mb-4">Collection 1</h3>
+                <div className="grid gap-2 p-2 grid-cols-2 sm:grid-cols-2 lg:gap-r lg:grid-cols-5">
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </section>
+            <section className="max-w-[1300px] mx-auto my-12">
+                <h3 className="text-primary font-heading font-bold text-center text-2xl  mb-4">Collection 2</h3>
+                <div className="grid gap-2 p-2 grid-cols-2 sm:grid-cols-2 lg:gap-r lg:grid-cols-5">
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </section>
+        </div>);
 }
--- src/components/product.js
+++ src/components/product.js
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function Product() {
     return (
-        <div className="flex flex-col justify-between bg-white border border-stone-300 rounded-lg min-h-full relative cursor-pointer hover:border-primary transition-all hover:shadow-md group">
+        <Link href={'/products/prod1'} className="flex flex-col justify-between bg-white border border-stone-300 rounded-lg min-h-full relative cursor-pointer hover:border-primary transition-all hover:shadow-md group">
 
 
-            <a href="https://lqrs.com/brands/bushmills/" className='overflow-hidden'>
+            <div href="https://lqrs.com/brands/bushmills/" className='overflow-hidden'>
                 <img className=" rounded-t-lg  w-full group-hover:scale-105 duration-150 transition-all" src="/eth1.webp" alt="Bushmills Black Bush Irish Whiskey" />
-            </a>
+            </div>
             <div className="flex-grow p-4 items-center flex justify-center">
 
                 <p className="sm:text-lg leading-5 sm:leading-6 text-gray-800 font-sans md:text-sm">Bushmills Black Irish
@@ -19,6 +19,6 @@ export default function Product() {
             <div className=" px-4 pb-4 items-center flex justify-center">
                 <p className='font-heading font-extrabold font-primary'>INR 3,995</p>
             </div>
-        </div>
+        </Link>
     )
 }
